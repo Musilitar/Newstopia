5c6f91a3d71d174f9cfdb176fe51f6f6be459e55
Lode Lesage <musilitar@gmail.com>
2014-03-19 15:49:34 +0100

    authentication

--- newstopia/auth/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
-
-
-class Contributor(AbstractBaseUser):
-    email = models.CharField(max_length=100, primary_key=True)
-    USERNAME_FIELD = 'email'
