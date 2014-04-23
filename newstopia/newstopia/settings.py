9e2f4b8895875a666ec20a775da4336e323d0129
Lode Lesage <musilitar@gmail.com>
2014-04-23 15:51:40 +0200

    static

--- newstopia/newstopia/settings.py
+++ newstopia/newstopia/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'articles',
     'authentication',
-    'tags'
+    'tags',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -85,5 +85,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILE_DIRS = (
+   BASE_DIR + '/static/'
+)
 
 AUTH_USER_MODEL = 'authentication.Contributor'
--- newstopia/newstopia/urls.py
+++ newstopia/newstopia/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,4 +10,4 @@ urlpatterns = patterns('',
     url(r'^account/', include('authentication.urls', namespace="account")),
     url(r'^tags/', include('tags.urls', namespace="tags")),
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
