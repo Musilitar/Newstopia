29e2f1047b5414a88ae82aeb969ea3a7c49ed870
Emmanuel <emmanuel.vandenbergh@student.ehb.be>
2014-05-04 23:19:08 +0200

    - Masonry zonder opmaak (hoera)

--- newstopia/static/js/main.js
+++ newstopia/static/js/main.js
@@ -1,12 +1,33 @@
 // JavaScript Document
 
-function setDate() {
+$(document).ready(function() {
     var months = [
     'Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli',
     'Augustus', 'September', 'Oktober', 'November', 'December'
     ];
-    var tomorrow = new Date();
-    document.getElementById('date').innerHTML = tomorrow.getDate() + " " + months[tomorrow.getMonth()] + " " + tomorrow.getFullYear();
-}
+    var today = new Date();
+    document.getElementById('date').innerHTML = today.getDate() + " " + months[today.getMonth()] + " " + today.getFullYear();
+});
 
-window.onload = setDate;
+
+$(document).ready(function() {
+
+  var container = document.querySelector('.masonry');
+  var msnry = new Masonry( container, {
+    columnWidth: 60
+  });
+
+  eventie.bind( container, 'click', function( event ) {
+    // don't proceed if item content was not clicked on
+    var target = event.target;
+    if ( !classie.has( target, 'item-content' )  ) {
+      return;
+    }
+    var itemElem = target.parentNode;
+    classie.toggleClass( itemElem, 'is-expanded' );
+
+    msnry.layout();
+    console.log("clicked upon");
+  });
+
+});
