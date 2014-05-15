bce101232f088e24cfcbd053cc540706259e5f17
Emmanuel <emmanuel.vandenbergh@student.ehb.be>
2014-05-15 15:31:31 +0200

    - articleBtns - LoginForm

--- newstopia/static/js/index.js
+++ newstopia/static/js/index.js
@@ -1,26 +1,24 @@
 // JavaScript Document
-
-        $(document).ready(function() {
+$(document).ready(function () {
 
     var container = document.querySelector('.masonry');
     var msnry = new Masonry(container, {
         columnWidth: 90
     });
 
-  eventie.bind( container, 'click', function( event ) {
-    // don't proceed if item content was not clicked on
-    var target = event.target;
-    if ( !classie.has( target, 'item-content' )  ) {
-      return;
-    }
-    var itemElem = target.parentNode;
-    classie.toggleClass( itemElem, 'is-expanded' );
-    if(jQuery(target).children('.paragraphRest').css('display') == 'none') {
-        jQuery(target).children('.paragraphRest').css('display', 'inline');
-    }
-    else {
-        jQuery(target).children('.paragraphRest').css("display", "none");
-    }
-    msnry.layout();
+    eventie.bind(container, 'click', function (event) {
+        // don't proceed if item content was not clicked on
+        var target = event.target;
+        if (!classie.has(target, 'btnRead')) {
+            return;
+        }
+        var itemElem = target.parentNode;
+        classie.toggleClass(itemElem, 'is-expanded');
+        if (jQuery(target).children('.paragraphRest').css('display') == 'none') {
+            jQuery(target).children('.paragraphRest').css('display', 'inline');
+        } else {
+            jQuery(target).children('.paragraphRest').css("display", "none");
+        }
+        msnry.layout();
+    });
 });
-        });
