6bfa694112e927d1fb896a305c0f2cfef426e0eb
Lode Lesage <musilitar@gmail.com>
2014-05-15 13:32:34 +0200

    jquery voor tonen van alle paragrafen werkt

--- newstopia/static/js/index.js
+++ newstopia/static/js/index.js
@@ -2,27 +2,26 @@
 
 $(document).ready(function() {
 
-    var container = document.querySelector('.masonry');
-    var msnry = new Masonry( container, {
-        columnWidth: 90
-    });
+  var container = document.querySelector('.masonry');
+  var msnry = new Masonry( container, {
+    columnWidth: 90
+  });
 
-    eventie.bind( container, 'click', function( event ) {
-        // don't proceed if item content was not clicked on
-        var target = event.target;
-        if ( !classie.has( target, 'item-content' )  ) {
-            return;
-        }
-        var itemElem = target.parentNode;
-        classie.toggleClass( itemElem, 'is-expanded' );
-        var paragraphElem = document.getElementsByClassName('paragraphRest');
-        for(var i = 0; i < paragraphElem.length; i++) {
-            if(jQuery(paragraphElem[i]).parents().hasClass('is-expanded')) {
-                classie.toggleClass( paragraphElem[i], 'is-shown' );
-            }
-        }
+  eventie.bind( container, 'click', function( event ) {
+    // don't proceed if item content was not clicked on
+    var target = event.target;
+    if ( !classie.has( target, 'item-content' )  ) {
+      return;
+    }
+    var itemElem = target.parentNode;
+    classie.toggleClass( itemElem, 'is-expanded' );
+    if(jQuery(target).children('.paragraphRest').css('display') == 'none') {
+        jQuery(target).children('.paragraphRest').css('display', 'inline');
+    }
+    else {
+        jQuery(target).children('.paragraphRest').css("display", "none");
+    }
+    msnry.layout();
+  });
 
-        msnry.layout();
-
-    });
 });
