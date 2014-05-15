e4355b0cadfdad95c6ca760ca16835e40d7c4a77
Emmanuel <emmanuel.vandenbergh@student.ehb.be>
2014-05-15 17:12:30 +0200

    - merge

--- newstopia/static/js/index.js
+++ newstopia/static/js/index.js
@@ -6,18 +6,15 @@ $(document).ready(function () {
         columnWidth: 90
     });
 
-    eventie.bind(container, 'click', function (event) {
-        // don't proceed if item content was not clicked on
+    $('.btnRead').click(function(event){
         var target = event.target;
-        if (!classie.has(target, 'btnRead')) {
-            return;
-        }
-        var itemElem = target.parentNode;
+        var itemElem = target.parentNode.parentNode.parentNode.parentNode;
         classie.toggleClass(itemElem, 'is-expanded');
-        if (jQuery(target).children('.paragraphRest').css('display') == 'none') {
-            jQuery(target).children('.paragraphRest').css('display', 'inline');
+
+        if (jQuery(target).parent().parent().parent().children('.paragraphRest').css('display') == 'none') {
+            jQuery(target).parent().parent().parent().children('.paragraphRest').css('display', 'inline');
         } else {
-            jQuery(target).children('.paragraphRest').css("display", "none");
+            jQuery(target).parent().parent().parent().children('.paragraphRest').css("display", "none");
         }
         msnry.layout();
     });
