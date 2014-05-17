699aed5850194a827310a58d3afebfc432c660c4
Emmanuel <emmanuel.vandenbergh@student.ehb.be>
2014-05-17 03:07:27 +0200

    - more/less - cursor article buttons - JS opkuisen

--- newstopia/static/js/index.js
+++ newstopia/static/js/index.js
@@ -14,11 +14,27 @@ $(document).ready(function () {
         var itemElem = target.parentNode.parentNode.parentNode.parentNode;
         classie.toggleClass(itemElem, 'is-expanded');
 
+        //set text margin
+        if (jQuery(target).parent().parent().parent().parent().hasClass('is-expanded')){
+            jQuery(target).parent().parent().parent().css( 'height', '95%');
+        } else {
+            jQuery(target).parent().parent().parent().css( 'height', '265px');
+        }
+
+        //load new paragraphs
         if (jQuery(target).parent().parent().parent().children('.paragraphRest').css('display') == 'none') {
             jQuery(target).parent().parent().parent().children('.paragraphRest').css('display', 'block');
         } else {
             jQuery(target).parent().parent().parent().children('.paragraphRest').css("display", "none");
         }
+
+        //change more button
+        if (jQuery(target).parent().parent().parent().parent().hasClass('is-expanded')){
+        $(target).html('<i class="fa fa-arrow-circle-up"></i> less');
+        } else {
+        $(target).html('<i class="fa fa-arrow-circle-down"></i> more');
+        }
+
         msnry.layout();
     });
 });
